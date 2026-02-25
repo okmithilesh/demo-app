789a17115d9f5ce14a10a34acab3e7e23a2bb63a
okmithilesh <okmithilesh@gmail.com>
2026-02-25 18:29:04 +0000

    Initial Java app with Dockerfile

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM eclipse-temurin:17-jre-jammy
+
+WORKDIR /app
+
+COPY target/demo-app-1.0-SNAPSHOT.jar app.jar
+
+EXPOSE 8080
+
+ENTRYPOINT ["java", "-jar", "app.jar"]
+
---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import com.sun.net.httpserver.HttpServer;
+
+public class App {
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/", exchange -> {
+            String response = "CI/CD Java App Running!";
+            exchange.sendResponseHeaders(200, response.length());
+            OutputStream os = exchange.getResponseBody();
+            os.write(response.getBytes());
+            os.close();
+        });
+        server.start();
+        System.out.println("Server started on port 8080");
+    }
+}
+
